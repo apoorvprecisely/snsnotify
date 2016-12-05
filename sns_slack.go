17f3c6b87409898faa7f32fa495e40d87d08a012
apoorvprecisely <apoorvqwerty@gmail.com>
2016-12-05 19:28:58 +0530

    feat(slack notification): add code to send notification to slack
    add code to send notification to slack

---
+++ sns_slack.go
@@ -0,0 +1,40 @@
+package snsnotify
+
+import (
+	"encoding/json"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sns"
+	ew "github.com/hashicorp/errwrap"
+)
+
+type SlackMessage struct {
+	Content     string `json:"content"`
+	Channel     string `json:"channel"`
+	Webhookpath string `json:"webhookpath"`
+}
+
+func NotifySlack(awsRegion string, awsWebhook string, slackCh string, slackArn string, subject string, message string) error {
+	svc := sns.New(session.New(aws.NewConfig().WithRegion(awsRegion)))
+	smsg := &SlackMessage{Channel: slackCh, Webhookpath: awsWebhook, Content: message}
+	b, err := json.Marshal(smsg)
+
+	if err != nil {
+		return ew.Wrapf("Error while generating message : {{err}}",
+			err)
+	}
+
+	params := &sns.PublishInput{
+		Message:  aws.String(string(b)),
+		TopicArn: aws.String(slackArn),
+		Subject:  aws.String(subject),
+	}
+
+	_, err = svc.Publish(params)
+
+	if err != nil {
+		return ew.Wrapf("Error while notifying slack : {{err}}",
+			err)
+	}
+	return nil
+}
